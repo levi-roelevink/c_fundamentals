95a46f12e8d6d6752390f3e8890390afa554fcf8
Levi Roelevink <538253@student.saxion.nl>
2025-10-14 13:52:30 +0200

    Modifiable character array

--- main.c
+++ main.c
@@ -2,28 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 
-// 👉 First, build and run the program.
-//
-// To do this, make sure you're in the `exercises` directory, and then run:
-//
-// gcc -o app2 2.c && ./app2
+// Returns the memory address of the actual path
+// char *to_path(char *req) {
+//     // Input: "GET /blog HTTP/1.1..."
+//     // Goal: "blog/index.html"
+//     char *start = req;
+// }
 
 int main() {
-    char *header = "HTTP/1.1 200 OK";
+    // char *req = "GET /blog HTTP/1.1...";
+    // char *path = to_path(req);
+    //
+    // printf("Path: %s", path);
 
-    // 👉 Try replacing this `15` with a call to `strlen` (and `#include <string.h>` above!)
-    write(1, header, strlen(header));
+    char header[50];
+    strcpy(header, "HTTP 200 OK");
 
-    // 👉 After you're using `strlen` above, try adding a `\0` (with the backslash)
-    // inside the definition of `header`, e.g.  "HTT\0P/1.1 200 OK" - this inserts
-    // a zero byte in the string. Before you run the program, what do you think it will print?
+    char *pointer = header;
 
-    // 👉 Try changing this `%s` to `%zu` (ignore the compiler warning).
-    printf("\n\nThat output was from write(). This is from printf: %s\n", (char*)0);
-
-    // 👉 Try changing the `%zud` back to `%s` and then replace the printf call's
-    // last argument (originally `header`) with this argument instead: `(char*)123456`
-    // 👉 Then try it with the number 0 instead of 123456.
+    strcpy(header, "HTTP 404 NOT OK");
 
     return 0;
 }
