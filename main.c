f0c0cf85c0a461d59fd227005fe03d75bd043bd6
Levi Roelevink <538253@student.saxion.nl>
2025-10-14 14:48:10 +0200

    Extract path from URL request

--- main.c
+++ main.c
@@ -1,26 +1,50 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 // Returns the memory address of the actual path
-// char *to_path(char *req) {
-//     // Input: "GET /blog HTTP/1.1..."
-//     // Goal: "blog/index.html"
-//     char *start = req;
-// }
+// Input: "GET /blog HTTP/1.1..."
+// Goal: "blog/index.html"
+char *to_path(const char *req) {
+    const int length = strlen(req);
+
+    int startIndex = 0;
+    int endIndex = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (req[i] == '/') {
+            startIndex = i + 1;
+            for (int j = startIndex; j < length; j++) {
+                if (req[j] == ' ') {
+                    endIndex = j - 1;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    const char *suffix = "/index.html";
+    const int pathLength = endIndex - startIndex + 1;
+    const int totalLength = pathLength + strlen(suffix);
+    char *result = malloc(totalLength);
+
+    // src: index in memory of first byte we want to copy
+    strncpy(result, req + startIndex, pathLength);
+    strcat(result, suffix);
+    return result; // Caller must free()
+}
 
 int main() {
-    // char *req = "GET /blog HTTP/1.1...";
-    // char *path = to_path(req);
-    //
-    // printf("Path: %s", path);
+    char *req = "GET /blog HTTP/1.1...";
+    char *path = to_path(req);
 
-    char header[50];
-    strcpy(header, "HTTP 200 OK");
-
-    char *pointer = header;
-
-    strcpy(header, "HTTP 404 NOT OK");
+    if (path) {
+        printf("Path: %s\n", path);
+        // Frees the memory allocated on the heap by malloc in the to_path function
+        free(path);
+    }
 
     return 0;
 }
